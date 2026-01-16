0db8de4169e60d884908354cf5fe627d729ca6cc
Frederick Morlock <frederickgeek8@gmail.com>
2026-01-16 17:49:38 -0500

    Fix dataworker crash
    Simply by wrapping the running script in `if __name__ == '__main__':`
    Luckily, this is the sort of thing where the fix appears in the error
    message.

--- examples/visualize_cnn_random.py
+++ examples/visualize_cnn_random.py
@@ -7,57 +7,58 @@ import splinecam as sc
 import torch
 import matplotlib.pyplot as plt
 
-### define model
+if __name__ == '__main__':
+    ### define model
 
-model = torch.nn.Sequential(
-    *[
-        torch.nn.Conv2d(3, 6, 5, stride=2, padding=2, bias=False),
-        torch.nn.BatchNorm2d(6),
-        torch.nn.ReLU(),
-        torch.nn.Conv2d(6, 16, 5, stride=2, padding=2, bias=False),
-        torch.nn.ReLU(),
-        torch.nn.Flatten(),
-        torch.nn.Linear(16 * 8 * 8, 120),
-        torch.nn.Linear(120, 84),
-        torch.nn.Linear(84, 10)
-    ]
-)
+    model = torch.nn.Sequential(
+        *[
+            torch.nn.Conv2d(3, 6, 5, stride=2, padding=2, bias=False),
+            torch.nn.BatchNorm2d(6),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(6, 16, 5, stride=2, padding=2, bias=False),
+            torch.nn.ReLU(),
+            torch.nn.Flatten(),
+            torch.nn.Linear(16 * 8 * 8, 120),
+            torch.nn.Linear(120, 84),
+            torch.nn.Linear(84, 10)
+        ]
+    )
 
-model.eval()
-model.type(torch.float64)
+    model.eval()
+    model.type(torch.float64)
 
-### define input domain to compute partitions
+    ### define input domain to compute partitions
 
-# prescribe input domain for partition computation
-domain = sc.utils.get_square_slice_from_one_anchor(torch.randn(1,3*32*32),
-                                                   pad_dist=2,
-                                                   seed=None)
+    # prescribe input domain for partition computation
+    domain = sc.utils.get_square_slice_from_one_anchor(torch.randn(1,3*32*32),
+                                                    pad_dist=2,
+                                                    seed=None)
 
-# compute linear projection from input space to target domain
-T = sc.utils.get_proj_mat(domain)
+    # compute linear projection from input space to target domain
+    T = sc.utils.get_proj_mat(domain)
 
-# wrap model with splinecam library
-NN = sc.wrappers.model_wrapper(
-    model,
-    input_shape=(3,32,32),
-    T = T,
-    dtype = torch.float64
-)
+    # wrap model with splinecam library
+    NN = sc.wrappers.model_wrapper(
+        model,
+        input_shape=(3,32,32),
+        T = T,
+        dtype = torch.float64
+    )
 
-print('forward and affine equivalency flag ', NN.verify())
+    print('forward and affine equivalency flag ', NN.verify())
 
-### Compute regions and decision boundary
+    ### Compute regions and decision boundary
 
-out_cyc,endpoints,Abw = sc.compute.get_partitions_with_db(domain,T,NN)
+    out_cyc,endpoints,Abw = sc.compute.get_partitions_with_db(domain,T,NN)
 
-### Plot partitions
+    ### Plot partitions
 
-minval,_ = torch.vstack(out_cyc).min(0)
-maxval,_ = torch.vstack(out_cyc).max(0)
+    minval,_ = torch.vstack(out_cyc).min(0)
+    maxval,_ = torch.vstack(out_cyc).max(0)
 
-sc.plot.plot_partition(out_cyc, xlims=[minval[0],maxval[0]],alpha=0.3,
-                         edgecolor='#a70000',color_range=[.3,.8],
-                         colors=['#469597', '#5BA199', '#BBC6C8', '#E5E3E4', '#DDBEAA'],
-                         ylims=[minval[1],maxval[1]], linewidth=.5)
+    sc.plot.plot_partition(out_cyc, xlims=[minval[0],maxval[0]],alpha=0.3,
+                            edgecolor='#a70000',color_range=[.3,.8],
+                            colors=['#469597', '#5BA199', '#BBC6C8', '#E5E3E4', '#DDBEAA'],
+                            ylims=[minval[1],maxval[1]], linewidth=.5)
 
-plt.savefig('../figures/cnn_visualize.jpg',transparent=True, bbox_inches=0, pad_inches=0)
+    plt.savefig('../figures/cnn_visualize.jpg',transparent=True, bbox_inches=0, pad_inches=0)
--- examples/visualize_mlp_random.py
+++ examples/visualize_mlp_random.py
@@ -9,66 +9,66 @@ import splinecam as sc
 import torch
 import matplotlib.pyplot as plt
 
-### model definition
+if __name__ == '__main__':
+    ### model definition
+    in_shape = 10
+    out_shape = 1
+    width = 20
+    depth = 10
+    # act = torch.nn.ReLU(inplace=True)
+    act = torch.nn.LeakyReLU(0.03)
 
-in_shape = 10
-out_shape = 1
-width = 20
-depth = 10
-# act = torch.nn.ReLU(inplace=True)
-act = torch.nn.LeakyReLU(0.03)
+    layers = []
 
-layers = []
-
-layers.append(torch.nn.Linear(in_shape,width))
-layers.append(act)
-
-
-for i in range(depth-1):
-    layers.append(torch.nn.Linear(width,width))
-    layers.append(torch.nn.BatchNorm1d(width))
+    layers.append(torch.nn.Linear(in_shape,width))
     layers.append(act)
-    
-layers.append(torch.nn.Linear(width,out_shape))
 
-model = torch.nn.Sequential(*layers)
-model.to(sc.utils.DEFAULT_DEVICE)
 
-model.eval()
-model.type(torch.float64)
+    for i in range(depth-1):
+        layers.append(torch.nn.Linear(width,width))
+        layers.append(torch.nn.BatchNorm1d(width))
+        layers.append(act)
+        
+    layers.append(torch.nn.Linear(width,out_shape))
 
-### define input domain to compute partitions
+    model = torch.nn.Sequential(*layers)
+    model.to(sc.utils.DEFAULT_DEVICE)
 
-# prescribe input domain for partition computation
-domain = sc.utils.get_square_slice_from_one_anchor(torch.randn(1,in_shape),
-                                                   pad_dist=2,
-                                                   seed=None)
+    model.eval()
+    model.type(torch.float64)
 
-# compute linear projection from input space to target domain
-T = sc.utils.get_proj_mat(domain)
+    ### define input domain to compute partitions
 
-# wrap model with splinecam library
-NN = sc.wrappers.model_wrapper(
-    model,
-    input_shape=(in_shape,),
-    T = T,
-    dtype = torch.float64
-)
+    # prescribe input domain for partition computation
+    domain = sc.utils.get_square_slice_from_one_anchor(torch.randn(1,in_shape),
+                                                    pad_dist=2,
+                                                    seed=None)
 
-print('forward and affine equivalency flag ', NN.verify())
+    # compute linear projection from input space to target domain
+    T = sc.utils.get_proj_mat(domain)
 
-### Compute regions and decision boundary
+    # wrap model with splinecam library
+    NN = sc.wrappers.model_wrapper(
+        model,
+        input_shape=(in_shape,),
+        T = T,
+        dtype = torch.float64
+    )
 
-out_cyc,endpoints,Abw = sc.compute.get_partitions_with_db(domain,T,NN)
+    print('forward and affine equivalency flag ', NN.verify())
 
-### Plot partitions
+    ### Compute regions and decision boundary
 
-minval,_ = torch.vstack(out_cyc).min(0)
-maxval,_ = torch.vstack(out_cyc).max(0)
+    out_cyc,endpoints,Abw = sc.compute.get_partitions_with_db(domain,T,NN)
 
-sc.plot.plot_partition(out_cyc, xlims=[minval[0],maxval[0]],alpha=0.3,
-                         edgecolor='#a70000',color_range=[.3,.8],
-                         colors=['#469597', '#5BA199', '#BBC6C8', '#E5E3E4', '#DDBEAA'],
-                         ylims=[minval[1],maxval[1]], linewidth=.5)
+    ### Plot partitions
 
-plt.savefig('../figures/mlp_visualize.jpg',transparent=True, bbox_inches=0, pad_inches=0)
+    minval,_ = torch.vstack(out_cyc).min(0)
+    maxval,_ = torch.vstack(out_cyc).max(0)
+
+    sc.plot.plot_partition(out_cyc, xlims=[minval[0],maxval[0]],alpha=0.3,
+                            edgecolor='#a70000',color_range=[.3,.8],
+                            colors=['#469597', '#5BA199', '#BBC6C8', '#E5E3E4', '#DDBEAA'],
+                            ylims=[minval[1],maxval[1]], linewidth=.5)
+
+    plt.savefig('../figures/mlp_visualize.jpg',transparent=True, bbox_inches=0, pad_inches=0)
